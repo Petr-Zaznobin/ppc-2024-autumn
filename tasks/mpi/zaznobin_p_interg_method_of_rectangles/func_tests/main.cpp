0977107ecdea0bd42ff36bf3bb4e2d46d95827e3
Petr Zaznobin <69428320+Petr-Zaznobin@users.noreply.github.com>
2024-11-03 21:58:25 +0300

    delete main()

--- tasks/mpi/zaznobin_p_interg_method_of_rectangles/func_tests/main.cpp
+++ tasks/mpi/zaznobin_p_interg_method_of_rectangles/func_tests/main.cpp
@@ -190,13 +190,3 @@ TEST(zaznobin_p_interg_method_of_rectangles_mpi, degree_mpi) {
   }
 }
 
-int main(int argc, char** argv) {
-  boost::mpi::environment env(argc, argv);
-  boost::mpi::communicator world;
-  ::testing::InitGoogleTest(&argc, argv);
-  ::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
-  if (world.rank() != 0) {
-    delete listeners.Release(listeners.default_result_printer());
-  }
-  return RUN_ALL_TESTS();
-}
