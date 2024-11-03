233dfd905e97eec55f7b2b4c385505fb812bf697
Petr Zaznobin <69428320+Petr-Zaznobin@users.noreply.github.com>
2024-11-03 20:24:18 +0300

    Update main.cpp

--- tasks/mpi/zaznobin_p_interg_method_of_rectangles/func_tests/main.cpp
+++ tasks/mpi/zaznobin_p_interg_method_of_rectangles/func_tests/main.cpp
@@ -3,8 +3,9 @@
 
 #include <boost/mpi/communicator.hpp>
 #include <boost/mpi/environment.hpp>
-#include <vector>
 #include <cmath>
+#include <vector>
+
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
