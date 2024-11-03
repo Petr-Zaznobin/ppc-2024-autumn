ffdf70447c273b934e4b68409d043c9a1db6c571
Petr Zaznobin <69428320+Petr-Zaznobin@users.noreply.github.com>
2024-11-03 20:33:06 +0300

    Update ops_seq.hpp

--- tasks/seq/example/include/ops_seq.hpp
+++ tasks/seq/example/include/ops_seq.hpp
@@ -11,13 +11,13 @@ namespace nesterov_a_test_task_seq {
 class TestTaskSequential : public ppc::core::Task {
  public:
   explicit TestTaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
-  bool pre_processing() override;  // Обработать данные так, как мы хотим видеть в нашей программе
-  bool validation() override;  // Проверка на адектватность
-  bool run() override;         // Тело программы
-  bool post_processing() override;  // Выдать результаты в удобоваримом виде для пользователя
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
 
  private:
   int input_{}, res{};
 };
 
-}  // namespace zaznobin_p_interg_method_of_rectangles_seq
+}  // namespace nesterov_a_test_task_seq
