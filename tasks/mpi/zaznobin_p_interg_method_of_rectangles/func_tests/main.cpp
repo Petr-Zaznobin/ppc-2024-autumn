5c7168b29809e902518ac2c3d5de1c17cc50f838
Petr Zaznobin <69428320+Petr-Zaznobin@users.noreply.github.com>
2024-11-07 01:47:07 +0300

    Update main.cpp

--- tasks/mpi/zaznobin_p_interg_method_of_rectangles/func_tests/main.cpp
+++ tasks/mpi/zaznobin_p_interg_method_of_rectangles/func_tests/main.cpp
@@ -1,4 +1,3 @@
-// Copyright 2024 Lupsha Egor
 #include <gtest/gtest.h>
 
 #include <boost/mpi/communicator.hpp>
@@ -7,7 +6,7 @@
 #include <random>
 #include <vector>
 
-#include "mpi/lupsha_e_rect_integration/include/ops_mpi.hpp"
+#include "mpi/zaznobin_p_interg_method_of_rectangles/include/ops_mpi.hpp"
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
@@ -26,7 +25,7 @@ std::tuple<double, double, int> generate_random_data() {
   return std::make_tuple(lower_bound, upper_bound, num_intervals);
 }
 
-TEST(lupsha_e_rect_integration_mpi, Test_Constant) {
+TEST(zaznobin_p_interg_method_of_rectangles_mpi, Test_Constant) {
   boost::mpi::communicator world;
   double lower_bound = 0.0;
   double upper_bound = 1.0;
@@ -44,7 +43,7 @@ TEST(lupsha_e_rect_integration_mpi, Test_Constant) {
     taskDataPar->outputs_count.emplace_back(global_sum.size());
   }
 
-  lupsha_e_rect_integration_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+  zaznobin_p_interg_method_of_rectangles_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
 
   std::function<double(double)> f = [](double x) { return 10.0; };
   testMpiTaskParallel.function_set(f);
@@ -65,7 +64,7 @@ TEST(lupsha_e_rect_integration_mpi, Test_Constant) {
     taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(result_seq.data()));
     taskDataSeq->outputs_count.emplace_back(1);
 
-    lupsha_e_rect_integration_mpi::TestMPITaskSequential sequential_Task(taskDataSeq);
+    zaznobin_p_interg_method_of_rectangles_mpi::TestMPITaskSequential sequential_Task(taskDataSeq);
     sequential_Task.function_set([](double x) { return 10.0; });
 
     ASSERT_EQ(sequential_Task.validation(), true);
@@ -77,7 +76,7 @@ TEST(lupsha_e_rect_integration_mpi, Test_Constant) {
   }
 }
 
-TEST(lupsha_e_rect_integration_mpi, Test_Logarithm) {
+TEST(zaznobin_p_interg_method_of_rectangles_mpi, Test_Logarithm) {
   boost::mpi::communicator world;
   double lower_bound = 0.1;
   double upper_bound = 1.0;
@@ -95,7 +94,7 @@ TEST(lupsha_e_rect_integration_mpi, Test_Logarithm) {
     taskDataPar->outputs_count.emplace_back(global_sum.size());
   }
 
-  lupsha_e_rect_integration_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+  zaznobin_p_interg_method_of_rectangles_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
 
   std::function<double(double)> f = [](double x) { return std::log(x); };
   testMpiTaskParallel.function_set(f);
@@ -116,7 +115,7 @@ TEST(lupsha_e_rect_integration_mpi, Test_Logarithm) {
     taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(result_seq.data()));
     taskDataSeq->outputs_count.emplace_back(1);
 
-    lupsha_e_rect_integration_mpi::TestMPITaskSequential sequential_Task(taskDataSeq);
+    zaznobin_p_interg_method_of_rectangles_mpi::TestMPITaskSequential sequential_Task(taskDataSeq);
     sequential_Task.function_set([](double x) { return std::log(x); });
 
     ASSERT_EQ(sequential_Task.validation(), true);
@@ -128,7 +127,7 @@ TEST(lupsha_e_rect_integration_mpi, Test_Logarithm) {
   }
 }
 
-TEST(lupsha_e_rect_integration_mpi, Test_Gaussian) {
+TEST(zaznobin_p_interg_method_of_rectangles_mpi, Test_Gaussian) {
   boost::mpi::communicator world;
   double lower_bound = -1.0;
   double upper_bound = 1.0;
@@ -146,7 +145,7 @@ TEST(lupsha_e_rect_integration_mpi, Test_Gaussian) {
     taskDataPar->outputs_count.emplace_back(global_sum.size());
   }
 
-  lupsha_e_rect_integration_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+  zaznobin_p_interg_method_of_rectangles_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
 
   std::function<double(double)> f = [](double x) { return std::exp(-x * x); };
   testMpiTaskParallel.function_set(f);
@@ -167,7 +166,7 @@ TEST(lupsha_e_rect_integration_mpi, Test_Gaussian) {
     taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(result_seq.data()));
     taskDataSeq->outputs_count.emplace_back(1);
 
-    lupsha_e_rect_integration_mpi::TestMPITaskSequential sequential_Task(taskDataSeq);
+    zaznobin_p_interg_method_of_rectangles_mpi::TestMPITaskSequential sequential_Task(taskDataSeq);
     sequential_Task.function_set([](double x) { return std::exp(-x * x); });
 
     ASSERT_EQ(sequential_Task.validation(), true);
@@ -179,7 +178,7 @@ TEST(lupsha_e_rect_integration_mpi, Test_Gaussian) {
   }
 }
 
-TEST(lupsha_e_rect_integration_mpi, Test_Power) {
+TEST(zaznobin_p_interg_method_of_rectangles_mpi, Test_Power) {
   boost::mpi::communicator world;
   double lower_bound = 0.0;
   double upper_bound = 1.0;
@@ -197,7 +196,7 @@ TEST(lupsha_e_rect_integration_mpi, Test_Power) {
     taskDataPar->outputs_count.emplace_back(global_sum.size());
   }
 
-  lupsha_e_rect_integration_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+  zaznobin_p_interg_method_of_rectangles_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
 
   std::function<double(double)> f = [](double x) { return x * x; };
   testMpiTaskParallel.function_set(f);
@@ -218,56 +217,7 @@ TEST(lupsha_e_rect_integration_mpi, Test_Power) {
     taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(result_seq.data()));
     taskDataSeq->outputs_count.emplace_back(1);
 
-    lupsha_e_rect_integration_mpi::TestMPITaskSequential sequential_Task(taskDataSeq);
-    sequential_Task.function_set([](double x) { return x * x; });
-
-    ASSERT_EQ(sequential_Task.validation(), true);
-    sequential_Task.pre_processing();
-    sequential_Task.run();
-    sequential_Task.post_processing();
-
-    ASSERT_NEAR(global_sum[0], result_seq[0], 1e-2);
-  }
-}
-
-TEST(lupsha_e_rect_integration_mpi, Test_Power_Random) {
-  boost::mpi::communicator world;
-  auto [lower_bound, upper_bound, num_intervals] = generate_random_data();
-  std::vector<double> global_sum(1, 0.0);
-  std::vector<double> result_seq(1, 0.0);
-
-  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
-
-  if (world.rank() == 0) {
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&lower_bound));
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&upper_bound));
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_intervals));
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_sum.data()));
-    taskDataPar->outputs_count.emplace_back(global_sum.size());
-  }
-
-  lupsha_e_rect_integration_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
-
-  std::function<double(double)> f = [](double x) { return x * x; };
-  testMpiTaskParallel.function_set(f);
-
-  ASSERT_TRUE(testMpiTaskParallel.validation());
-  testMpiTaskParallel.pre_processing();
-  testMpiTaskParallel.run();
-  testMpiTaskParallel.post_processing();
-
-  if (world.rank() == 0) {
-    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&lower_bound));
-    taskDataSeq->inputs_count.emplace_back(1);
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&upper_bound));
-    taskDataSeq->inputs_count.emplace_back(1);
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_intervals));
-    taskDataSeq->inputs_count.emplace_back(1);
-    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(result_seq.data()));
-    taskDataSeq->outputs_count.emplace_back(1);
-
-    lupsha_e_rect_integration_mpi::TestMPITaskSequential sequential_Task(taskDataSeq);
+    zaznobin_p_interg_method_of_rectangles_mpi::TestMPITaskSequential sequential_Task(taskDataSeq);
     sequential_Task.function_set([](double x) { return x * x; });
 
     ASSERT_EQ(sequential_Task.validation(), true);
